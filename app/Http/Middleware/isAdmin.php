898c26c877c39275f0b20fb4290ab4fd4d330415
Zaid Hilman <mannmarpaung@gmail.com>
2024-04-24 13:42:23 +0700

    4

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class isAdmin
@@ -15,6 +16,12 @@ class isAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (Auth::user() && Auth::user()->role == 'admin') {
+            return view('pages.admin.index');
+        } else {
+            return view('pages.user.index');
+        }
+
         return $next($request);
     }
 }
