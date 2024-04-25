015704d27f0d752a708788af51920e17b9921800
Zaid Hilman <mannmarpaung@gmail.com>
2024-04-25 19:50:05 +0700

    2

--- resources/views/layouts/include/navbar.blade.php
+++ resources/views/layouts/include/navbar.blade.php
@@ -226,7 +226,7 @@
                     </li>
 
                 </ul><!-- End Profile Dropdown Items -->
-            </li><!-- End Profile Nav -->
+            </li><!-- End Profile Nav 2 -->
 
         </ul>
     </nav><!-- End Icons Navigation -->
