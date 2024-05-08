20f1d963a4b5193b3c8a2f92149d0e45f7be01cf
Zaid Hilman <mannmarpaung@gmail.com>
2024-05-08 13:52:16 +0700

    2

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -38,7 +38,7 @@ class RegisterController extends Controller
     }
 
     /**
-     * Create a new controller instance.
+     * Create a new controller instance.2
      *
      * @return void
      */
