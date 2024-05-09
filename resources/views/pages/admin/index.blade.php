56a072e158e6ffecfc0658f6b0b1010c7009475b
Zaid Hilman <mannmarpaung@gmail.com>
2024-05-09 13:24:55 +0700

    4

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -6,7 +6,7 @@
 
     <div class="section dashboard">
 
-        <!-- Customers Card 3-->
+        <!-- Customers Card 4-->
         <div class="col-xxl-4 col-xl-12">
 
             <div class="card info-card customers-card">
